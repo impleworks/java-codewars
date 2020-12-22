0654623b7df7dcb8b58688fab5c681e5a1ccab34
meltworks <76448415+meltworks@users.noreply.github.com>
2020-12-22 23:51:58 +0300

    Create AngleSum.java

---
+++ src/com/company/kata/kyu7/AngleSum.java
@@ -0,0 +1,13 @@
+package com.company.kata.kyu7;
+
+/**
+ * Sum of angles
+ * https://www.codewars.com/kata/5a03b3f6a1c9040084001765
+ * 22-12-2020
+ */
+
+public class AngleSum {
+    public static int sumOfAngles(int n) {
+        return 180 * (n - 2);
+    }
+}
